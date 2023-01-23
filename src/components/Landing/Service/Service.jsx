354a49bd8da46f35b006c5aa33e1be350bdee4e9
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2023-01-23 10:30:03 +0100

    Accordion details from json file

--- src/components/Landing/Service/Service.jsx
+++ src/components/Landing/Service/Service.jsx
@@ -1,5 +1,7 @@
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import s from './Service.module.css';
 
 const plusStyle = {
@@ -8,48 +10,29 @@ const plusStyle = {
 }
 
 const Service = () => {
+    const [content, setContent] = useState([]);
+
+    useEffect(() => {
+        fetch('./modelin/data/accordions.json')
+            .then(response => response.json())
+            .then(data => setContent(data))
+    }, [] )
+
   return (
       <div className={s.section}>
           <h2 className={s.title}>Services</h2>
-          <Accordion className={s.accordionFirst}>
-              <AccordionSummary className={s.accSum}
-                    expandIcon={<i className="fa-sharp fa-solid fa-plus" style={plusStyle}></i>}>
-                  <Typography className={s.sumTitle}>Design projects</Typography>
-              </AccordionSummary>
-              <AccordionDetails className={s.accDet}>
-                  <Typography className={s.sumText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui eros, aliquam vitae lorem id, bibendum vehicula nunc. Etiam efficitur euismod tortor, sit amet placerat nulla condimentum ac. Ut non leo vel magna sagittis pulvinar. Donec iaculis, odio sed bibendum blandit, nisl massa porta massa, nec mollis metus augue eget lectus. Nulla vitae nulla ligula. Sed leo erat, laoreet non tempor vel, dictum non lacus. Curabitur convallis ex ac condimentum consequat.</Typography>
-              </AccordionDetails>
-          </Accordion>
 
-          <Accordion className={s.accordion}>
+          {content.map((item) => 
+              <Accordion className={s.accordion} key={item.id}>
               <AccordionSummary className={s.accSum}
                     expandIcon={<i className="fa-sharp fa-solid fa-plus" style={plusStyle}></i>}>
-                  <Typography className={s.sumTitle}>Set and interior</Typography>
+                      <Typography className={s.sumTitle}>{item.title}</Typography>
               </AccordionSummary>
               <AccordionDetails className={s.accDet}>
-                  <Typography className={s.sumText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui eros, aliquam vitae lorem id, bibendum vehicula nunc. Etiam efficitur euismod tortor, sit amet placerat nulla condimentum ac. Ut non leo vel magna sagittis pulvinar. Donec iaculis, odio sed bibendum blandit, nisl massa porta massa, nec mollis metus augue eget lectus. Nulla vitae nulla ligula. Sed leo erat, laoreet non tempor vel, dictum non lacus. Curabitur convallis ex ac condimentum consequat.</Typography>
+                      <Typography className={s.sumText}>{item.description}</Typography>
               </AccordionDetails>
           </Accordion>
-
-          <Accordion className={s.accordion} sx={{'& .MuiPaper-root-MuiAccordion-root:before': {backgroundColor: '#red'} }}>
-              <AccordionSummary className={s.accSum}
-                    expandIcon={<i className="fa-sharp fa-solid fa-plus" style={plusStyle}></i>}>
-                  <Typography className={s.sumTitle}>Decoration</Typography>
-              </AccordionSummary>
-              <AccordionDetails className={s.accDet}>
-                  <Typography className={s.sumText}>Offering you a full range of interior and furniture design services. Experience of over 10 years of successful projects on creating unique and modern interior designs all over the world. In my projects I strive to create a perfect interior for you. I often ask about your habits, preferences, hobbies, and other activities.</Typography>
-              </AccordionDetails>
-          </Accordion>
-
-          <Accordion className={s.accordionLast}>
-              <AccordionSummary className={s.accSum}
-                    expandIcon={<i className="fa-sharp fa-solid fa-plus" style={plusStyle}></i>}>
-                  <Typography className={s.sumTitle}>Supervision</Typography>
-              </AccordionSummary>
-              <AccordionDetails className={s.accDet}>
-                  <Typography className={s.sumText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui eros, aliquam vitae lorem id, bibendum vehicula nunc. Etiam efficitur euismod tortor, sit amet placerat nulla condimentum ac. Ut non leo vel magna sagittis pulvinar. Donec iaculis, odio sed bibendum blandit, nisl massa porta massa, nec mollis metus augue eget lectus. Nulla vitae nulla ligula. Sed leo erat, laoreet non tempor vel, dictum non lacus. Curabitur convallis ex ac condimentum consequat.</Typography>
-              </AccordionDetails>
-          </Accordion>
+          )}
     </div>
   )
 }
